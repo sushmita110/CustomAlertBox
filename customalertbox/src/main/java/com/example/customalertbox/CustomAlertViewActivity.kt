9ff08a446c4959daf99b48048869f7aa2835121b
Sushmita Shrestha <sushmita@outcodesoftware.com>
2021-01-28 17:39:23 +0545

    added UI for the alertbox

---
+++ customalertbox/src/main/java/com/example/customalertbox/CustomAlertViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.customalertbox
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomAlertViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_custom_alert_view)
+    }
+}
